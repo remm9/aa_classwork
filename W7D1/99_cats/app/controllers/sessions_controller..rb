5983b090b92498cc426811b7573bafbbae81f8cb
roman <admin@MacBook-Pro-9.local>
2020-04-27 18:19:47 -0400

    99 cats project

---
+++ W7D1/99_cats/app/controllers/sessions_controller..rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+    # def new
+    #     render :new
+    # end
+
+    # def create
+    #     user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+    #     if user
+    #         login(user)
+    #         redirect_to cats_url
+    #     else
+    #         flash.now[:errors] = ["Wrong username/password combination"]
+    #         render :new
+    #     end
+    # end
+
+    def destroy
+        debugger
+        # logout
+        # redirect_to new_session_url
+    end
+    
+end
