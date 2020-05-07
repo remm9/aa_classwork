89f62ccde2cd15e5ffe6b41e3ed104832f2f95d6
roman <admin@MacBook-Pro-9.local>
2020-05-06 20:44:33 -0400

    Did sumRec exponent and fibonacci

--- W8D3/js_project/phase_4_recursion.js
+++ W8D3/js_project/phase_4_recursion.js
@@ -1,15 +1,22 @@
 const range = function(start, end) {
-    // debugger
 
-    if (start >= end) return [start];
-    range(start+1, end)
+    if (start === end) { return [] };
+    let r = range(start, end-1)
+    r.push(end-1)
+    return r
 
-    // return range
 }
 
-console.log(range(1, 5))
+// console.log(range(1, 5))
 
+const sumRec = (array) => {
+    if (array.length <= 1) { return array[0]};
 
+    let result = sumRec(array.slice(1, array.length));
+    return array[0] += result;
+}
+
+// console.log(sumRec([1,2,3,4,5]))
 
 
 // function range(start, end){
@@ -19,3 +26,53 @@ console.log(range(1, 5))
 // const range = (start, end) => {
 
 // }
+
+const exponent = (base, exp) => {
+    if (exp === 0) { 
+        return 1
+    } else if (exp === 1) { 
+        return base
+    }
+
+    if (exp % 2 === 0) { 
+        let result = exponent(base, exp/2) ** 2
+        return result
+    } else if (exp % 2 === 1) {
+        let result = base * (exponent(base, (exp-1) / 2 ) ** 2)
+        return  result
+    }
+
+}
+
+// const exponent = (base, exp) => {
+//     if (exp === 0) {
+//         return 1
+//     } else if (exp === 1) {
+//         return base
+//     }
+
+//     let result = exponent(base, exp - 1)
+//     return base * result
+
+// }
+
+// console.log(exponent(3, 2))
+// console.log(exponent(3, 3))
+
+const fibonacci = (n) => {
+    let fibs = [1, 1]
+
+    if (n === 1) {
+        return [1]
+    } else if (n === 2) {
+        return fibs
+    }
+
+    let result = fibonacci(n-1)
+    result.push(result[result.length-1] + result[result.length-2])
+
+
+    return result
+}
+
+console.log(fibonacci(5))
